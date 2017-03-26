e0254c27d916b64478a9ad9374786e9bae90bab5
Jan Waś <janek.jan@gmail.com>
2017-03-26 21:12:52 +0200

    started working on hangman

---
+++ hangman.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+os.system('cls')
+
+
+def pobieranie_i_weryfikacja():
+    slowo = 'samolot'
+
+    while True:
+        litera = raw_input("Wpisz pojedyncza litere: ")
+        # pkt 1*
+        # liczy czy faktycznie została wpisana pojedyncza litera
+
+        # walidacja
+        if len(litera) != 1:
+            print 'miala byc jedna litera'
+            continue
+        break
+
+    return slowo, litera
+
+
+def main():
+    odganiete = 0
+    while True:
+        slowo, litera = pobieranie_i_weryfikacja()
+
+        if litera not in slowo:
+            print '0 wystapien. idz umrzyj'
+            continue
+
+        # wykonanie
+        print 'brawo'
+        print slowo.count(litera), ('wystapienia')
+        odganiete = odganiete + slowo.count(litera)
+        if odganiete >= len(slowo):
+            print 'wygrales'
+            exit()
+
+
+if __name__ == '__main__':
+    # uruchom glowny program tylko jak ten skrypt jest odpalony z python a nie importowany z innego pliku
+    main()
