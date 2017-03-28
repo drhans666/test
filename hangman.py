78beeecdda6cd6cffdcf97b20527f53640575c9a
drhans666 <dirtruls666>
2017-03-29 00:24:09 +0200

    added "things to do at the end" + visibility of lifes left

--- hangman.py
+++ hangman.py
@@ -27,11 +27,12 @@ def main():
     odganiete = 0
     while True:
         slowo, litera = pobieranie_i_weryfikacja()
-
+        #wszystkie skutki pomylek
         if litera not in slowo:
             del zycia[1]
             print '0 wystapien. idz umrzyj'
-            if len(zycia) < 2:
+            print ('POZOSTALO', len(zycia), 'ZYC')
+            if len(zycia) < 1:
                 sys.exit('Przegrales')
             continue
 
@@ -47,3 +48,10 @@ def main():
 if __name__ == '__main__':
     # uruchom glowny program tylko jak ten skrypt jest odpalony z python a nie importowany z innego pliku
     main()
+
+
+#Do zrobienia:
+#1. losowe wybieranie słów
+#2. wyswietlanie odgadnietych liter
+#3. brak możliwości ponownego odgadniecia litery
+#4. Powiazac grafike wisielca z iloscia zyc
