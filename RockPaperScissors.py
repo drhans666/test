32d69cfcb3721de46131e41f73b36f65525e187f
drhans666 <k.krasowski87@gmail.com>
2017-04-04 19:28:40 +0200

    trying functions

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -48,7 +48,7 @@ def getComputerChoice():
 
     return choice2
 
-def assignChoicesToArena(arena):
+def assignChoicesToArena(arena,choice, choice2):
     arena.append(choice)
     print('Player:', ART[choice])
     arena.append(choice2)
