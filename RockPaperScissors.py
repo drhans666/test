8f65a496986af487b6d148b78cb718bb4adf63e5
drhans666 <k.krasowski87@gmail.com>
2017-04-04 22:17:13 +0200

    Almost done. -error in point summary -something not exactly right with exit

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,4 +1,6 @@
 import random
+import sys
+import os
 
 ART = ['''
     
@@ -48,33 +50,48 @@ def getComputerChoice():
 
     return choice2
 
-def assignChoicesToArena(arena,choice, choice2):
+
+def assignChoicesToArena(arena,choice,choice2):
     arena.append(choice)
     print('Player:', ART[choice])
     arena.append(choice2)
     print('Computer:', ART[choice2])
-    return choice, choice2, arena
+    arena = []
+    return arena
 
-def choiceConfront():
+
+
+
+
+def choiceConfront(arena,playerPoints, computerPoints):
+
     if arena == [0, 0]:
         print('draw')
     elif arena == [0, 1]:
         print('paper wins')
+        computerPoints += 1
     elif arena == [0, 2]:
         print('rock wins')
+        playerPoints += 1
     elif arena == [1, 0]:
         print('paper wins')
+        playerPoints += 1
     elif arena == [1, 1]:
         print('draw')
     elif arena == [1, 2]:
         print('scissors wins')
+        computerPoints += 1
     elif arena == [2, 0]:
         print('rock wins')
+        computerPoints += 1
     elif arena == [2, 1]:
         print('scissors wins')
+        playerPoints += 1
     elif arena == [2, 2]:
         print('draw')
 
+
+    return playerPoints, computerPoints
 
 
 def playAgain():
@@ -82,6 +99,28 @@ def playAgain():
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
-weapons = ['paper', 'rock', 'scissors']
-arena = ()
+os.system('clear')
+weapons = ['rock', 'paper', 'scissors']
+playerPoints = 0
+computerPoints = 0
 
+while True:
+    arena = []
+    playerPoints = choiceConfront(arena,playerPoints, computerPoints)
+    computerPoints = choiceConfront(arena,playerPoints, computerPoints)
+    choice = getPlayerChoice()
+    choice2 = getComputerChoice()
+    assignChoicesToArena(arena, choice, choice2)
+    choiceConfront(arena, playerPoints, computerPoints)
+    print('Player Points:',playerPoints)
+    print('Computer Points:', computerPoints)
+
+    if playerPoints == 3:
+        print('Congratulations, You won!')
+    elif computerPoints == 3:
+        print('You Lost. Better Luck Next Time!')
+        break
+
+    playAgain()
+
+sys.exit()
