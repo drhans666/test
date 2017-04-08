98d40fb7c7ab8c3ce2d9054f3fe8abc2366cebe1
drhans666 <k.krasowski87@gmail.com>
2017-04-08 16:56:16 +0200

    added movement limits on the borders of zone

--- ZoneMovementTryouts.py
+++ ZoneMovementTryouts.py
@@ -1,9 +1,11 @@
-
-zone = [['.', '.', '.', '.', '.', ],
-        ['.', '.', '.', '.', '.', ],
-        ['.', '.', '.', '.', '.', ],
-        ['.', '.', '.', '.', '.', ],
-        ['.', '.', '.', '.', '.', ]]
+import os
+zone = [['-', '-', '-', '-', '-', '-', '-'],
+        ['|', '.', '.', '.', '.', '.', '|'],
+        ['|', '.', '.', '.', '.', '.', '|'],
+        ['|', '.', '.', '.', '.', '.', '|'],
+        ['|', '.', '.', '.', '.', '.', '|'],
+        ['|', '.', '.', '.', '.', '.', '|'],
+        ['-', '-', '-', '-', '-', '-', '-']]
 
 
 #displays intro with moving object in the center
@@ -19,8 +21,8 @@ def movementY(coord, axisY):
         axisY -= 1
     elif coord == 'down':
         axisY += 1
-
     return axisY
+
 #horizontal movement
 def movementX(coord, axisX):
 
@@ -28,24 +30,54 @@ def movementX(coord, axisX):
         axisX -= 1
     elif coord == 'right':
         axisX += 1
+    return axisX
+#Limits horizontal movement in zone
+def limitY(axisY):
+    while True:
+        if axisY == limitMin:
+            axisY += 1
+        elif axisY == limitMax:
+            axisY -= 1
 
+        else:
+            break
+    return axisY
+
+#Limits vertical movement in zone
+def limitX(axisX):
+    while True:
+        if axisX == limitMin:
+            axisX += 1
+        elif axisX == limitMax:
+            axisX -= 1
+
+        else:
+            break
     return axisX
 
 
 
-axisY = 2
-axisX = 2
 
+
+axisY = 3
+axisX = 3
+limitMin = 0
+limitMax = len(zone) - 1
+os.system('clear')
 introScreen()
 
 while True:
-
     print('Type Your direction: up, down, left, right')
     coord = input()
+
+    os.system('clear')
     axisY = movementY(coord, axisY)
     axisX = movementX(coord, axisX)
+    axisY = limitY(axisY)
+    axisX = limitX(axisX)
 
     zone[axisY][axisX] = 'O'
+
     for i in zone:
         print(i)
 
