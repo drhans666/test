9de655dda9e58b13e8419db74374a383706e02f1
drhans666 <k.krasowski87@gmail.com>
2017-04-12 01:13:20 +0200

    Test exercise from "Automate the Boring Stuff With Python" book

---
+++ TestExerciseDragonLoot.py
@@ -0,0 +1,25 @@
+
+
+def displayInventory(inv):
+    print('inventory:')
+    item_total = 0
+    for item, numbers in inv.items():
+        print (str(numbers) + ' ' + item)
+        item_total += numbers
+    print('Total items: ' + str(item_total))
+
+def addToInventory(inv, addedItems):
+    addedItems = 0
+    for i in dragonLoot:
+        if i in inv:
+            inv[i] = inv[i] + 1
+        if i not in inv:
+            inv.setdefault(i, 1)
+
+    return(inv)
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
+
